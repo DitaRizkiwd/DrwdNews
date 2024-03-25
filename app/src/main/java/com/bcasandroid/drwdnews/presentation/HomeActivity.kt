b900061b5a7dd2defece02fb2c593c791edb587c
DitaRizkiwd <rdita201@gmail.com>
2024-03-25 15:22:26 +0700

    First commit

---
+++ app/src/main/java/com/bcasandroid/drwdnews/presentation/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.bcasandroid.drwdnews.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bcasandroid.drwdnews.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
